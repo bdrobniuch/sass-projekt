1360324bb3f3adea0facf85f6cf79ee187b16197
bdrobniuch <drobniuch@gmail.com>
2018-10-21 11:36:44 +0200

    gotowy do pracy gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssbeautify = require('gulp-cssbeautify');
+
 gulp.task('sass', function () {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass())
